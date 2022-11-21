eb5647271180a5137543cbd4e6664502c3c10aa3
AIMODEM <aijpshm@gmail.com>
2022-11-21 22:37:45 +0530

    commit #3 : Data transformation phase completed for sensor-fault-detection project.

--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -1,9 +1,11 @@
 import sys, os
 from sensor.components import data_ingestion
-from sensor.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig, DataValidationConfig
-from sensor.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
+from sensor.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig, DataValidationConfig, DataTransformationConfig
+from sensor.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact, DataTransformationArtifact
 from sensor.components.data_ingestion import DataIngestion
 from sensor.components.data_validation import DataValidation
+from sensor.components.data_transformation import DataTransformation
+
 
 
 from sensor.exception import SensorException
@@ -27,18 +29,27 @@ class TrainPipeline:
     
     def start_data_validation(self,data_ingestion_artifact:DataIngestionArtifact)->DataValidationArtifact:
         try:
+            logging.info("Starting data validation...")
             data_validation_config = DataValidationConfig(training_pipeline_config=self.training_pipeline_config)
             data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
                                             data_validation_config = data_validation_config
                                             )
             data_validation_artifact = data_validation.initiate_data_validation()
+            logging.info("Data validation completed.")
             return data_validation_artifact
         except Exception as e:
             raise SensorException(e, sys)
     
-    def start_data_transformation(self):
+    def start_data_transformation(self, data_validation_artifact:DataValidationArtifact):
         try:
-            pass
+            logging.info("Starting data transformation...")
+            data_transformation_config = DataTransformationConfig(training_pipeline_config=self.training_pipeline_config)
+            data_transformation = DataTransformation(data_validation_artifact=data_validation_artifact,
+            data_transformation_config=data_transformation_config
+            )
+            data_transformation_artifact =  data_transformation.initiate_data_transformation()
+            logging.info("Data transformation completed.")
+            return data_transformation_artifact
         except Exception as e:
             raise SensorException(e, sys)
 
@@ -64,5 +75,6 @@ class TrainPipeline:
         try:
            data_ingestion_artifact:DataIngestionArtifact = self.start_data_ingestion()
            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+           data_transformation_artifact = self.start_data_transformation(data_validation_artifact=data_validation_artifact)
         except Exception as e:
             raise SensorException(e, sys)
